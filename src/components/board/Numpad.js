a8cfa7dc23c2e152c85c50162205e9965146b2f4
vvyvi <ewawozny4@gmail.com>
2020-09-26 02:15:31 +0200

    init

--- src/components/board/Numpad.js
+++ src/components/board/Numpad.js
@@ -1,17 +1,20 @@
 import React from 'react'
+import Sudoku from '../../hooks/Sudoku.js'
 
 const Timer = () => {
+  const {selected, board, setBoard} = Sudoku()
+
   return(
     <div className="main_numpad">
-            <span>1</span>
-            <span>2</span>
-            <span>3</span>
-            <span>4</span>
-            <span>5</span>
-            <span>6</span>
-            <span>7</span>
-            <span>8</span>
-            <span>9</span>
+            <span onClick={handleClick}>1</span>
+            <span onClick={handleClick}>2</span>
+            <span onClick={handleClick}>3</span>
+            <span onClick={handleClick}>4</span>
+            <span onClick={handleClick}>5</span>
+            <span onClick={handleClick}>6</span>
+            <span onClick={handleClick}>7</span>
+            <span onClick={handleClick}>8</span>
+            <span onClick={handleClick}>9</span>
     </div>
   )
 }
